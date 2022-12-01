90a87a45364204d9d45e2a18220922b5c89d6f52
Andrzej Kaczorowski <andrzej.kaczorowski@cybercom.com>
2022-12-01 10:32:42 +0100

    guessGame implementation
    - implemenetd guessGame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello world");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Guess the number: ");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error during reading line");
+
+        let input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(err_val) => {
+                println!("incorrect value: {} \n Provide correct integer - error: ", err_val);
+                continue;
+            }
+        };
+
+        match input.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Won!");
+                break;
+            }
+            Ordering::Less => println!("Too small!"),
+        }
+    }
 }
